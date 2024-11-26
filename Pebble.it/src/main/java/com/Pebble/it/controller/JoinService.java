54b27ba60891e1d8ba9299f87d81933e1e45eeb9
dotoriberry <dotoriberry@gmail.com>
2024-11-26 14:32:28 +0900

    Revert "fix: 로그인 기능 동작하도록 오류 수정"
    This reverts commit e3b14f84b0e1073539971bc87589afbd9a307a0b.

--- Pebble.it/src/main/java/com/Pebble/it/controller/JoinService.java
+++ Pebble.it/src/main/java/com/Pebble/it/controller/JoinService.java
@@ -1,7 +1,6 @@
 package com.Pebble.it.controller;
 
 import java.io.IOException;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,16 +9,12 @@ import javax.servlet.http.HttpServletResponse;
 import com.Pebble.it.model.MemberDAO;
 import com.Pebble.it.model.MemberDTO;
 
-@WebServlet("/Join")
+@WebServlet("/JoinService")
 public class JoinService extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    	
-    	// 한글 인코딩
-    	request.setCharacterEncoding("UTF-8");
-    	
-    	// 요청 파라미터에서 데이터 가져오기
+        // 요청 파라미터에서 데이터 가져오기
         String id = request.getParameter("id");
         String pw = request.getParameter("pw");
         String name = request.getParameter("name");
@@ -28,27 +23,22 @@ public class JoinService extends HttpServlet {
         String sportLevel = request.getParameter("sportLevel");
         
         // DTO 생성
-        MemberDTO dto = new MemberDTO();
-		dto.setId(id);
-		dto.setPw(pw);
-		dto.setName(name);
-		dto.setProfileImg(profileImg);
-		dto.setFavoriteSport(favoriteSport);
-		dto.setSportLevel(sportLevel);
-		
+        MemberDTO member = new MemberDTO(id, pw, name, profileImg, favoriteSport, sportLevel);
+
         // DAO를 통해 회원가입 처리
         MemberDAO dao = new MemberDAO();
-        int result = dao.join(dto);
+        boolean result = dao.register(member);
 
         // 결과에 따른 페이지 이동
-        if (result > 0) {
+        if (result) {
             // 회원가입 성공: JavaScript를 통해 메시지 띄운 후 로그인 페이지로 이동
-			// HttpSession session = request.getSession();
-			// session.setAttribute("email", email);
-			
-			RequestDispatcher rd = request.getRequestDispatcher("home.jsp");
-			rd.forward(request, response);
-			
+            response.setContentType("text/html; charset=UTF-8");
+            response.getWriter().println(
+                "<script>" +
+                    "alert('회원가입이 성공적으로 완료되었습니다. 로그인 페이지로 이동합니다.');" +
+                    "location.href='index.jsp';" +
+                "</script>"
+            );
         } else {
             // 회원가입 실패: 회원가입 페이지로 리다이렉트
             response.sendRedirect("join.jsp?error=1");
--- Pebble.it/src/main/java/com/Pebble/it/model/MemberDTO.java
+++ Pebble.it/src/main/java/com/Pebble/it/model/MemberDTO.java
@@ -1,15 +1,5 @@
 package com.Pebble.it.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-
-//lombok 연결하기
-@Data
-@AllArgsConstructor // 전체 필드를 가지고 생성자 메소드를 생성!
-@NoArgsConstructor // 기본의 생성자 메소드를 생성!
-
 public class MemberDTO {
 	private String id;   // 아이디
 	private String pw;   // 비밀번호 
@@ -17,4 +7,69 @@ public class MemberDTO {
 	private String profileImg; // PROFILE_IMG
     private String favoriteSport; // FAVORITE_SPORT
     private String sportLevel;    // SPORT_LEVEL
+	
+    // 기본 생성자 
+    public MemberDTO() {}  
+    
+    // 모든 필드를 포함한 생성자
+    public MemberDTO(String id, String pw, String name, String profileImg, String favoriteSport, String sportLevel) {
+    	this.id = id;
+    	this.pw = pw;
+    	this.name = name;
+    	this.profileImg = profileImg;
+    	this.favoriteSport = favoriteSport;
+    	this.sportLevel = sportLevel;
+    }
+
+    // Getter & Setter
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPw() {
+		return pw;
+	}
+
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getProfileImg() {
+		return profileImg;
+	}
+
+	public void setProfileImg(String profileImg) {
+		this.profileImg = profileImg;
+	}
+
+	public String getFavoriteSport() {
+		return favoriteSport;
+	}
+
+	public void setFavoriteSport(String favoriteSport) {
+		this.favoriteSport = favoriteSport;
+	}
+
+	public String getSportLevel() {
+		return sportLevel;
+	}
+
+	public void setSportLevel(String sportLevel) {
+		this.sportLevel = sportLevel;
+	}
+	
+    
+
 }
